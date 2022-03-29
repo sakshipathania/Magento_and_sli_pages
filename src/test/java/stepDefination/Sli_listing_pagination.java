4618d25767ee27a75a986317f94f6fd983e8cc3a
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-03-29 13:25:11 +0530

    Update Sli_listing_pagination.java

--- src/test/java/stepDefination/Sli_listing_pagination.java
+++ src/test/java/stepDefination/Sli_listing_pagination.java
@@ -129,7 +129,7 @@ public class Sli_listing_pagination extends BaseClass {
 					.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='clicking']")))
 					.getText();
 			Assert.assertTrue("user is not on corect page",
-					verify_ppt_page.contains("Download this presentation"));
+					verify_ppt_page.contains("Download This Document"));
 			Thread.sleep(2000);
 		} catch (NoSuchElementException e) {
 
