d75ee77089c69b70855ac07badbb94a108871733
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-05-17 18:13:54 +0530

    Update Free_Magento_pages.java

--- src/test/java/stepDefination/Free_Magento_pages.java
+++ src/test/java/stepDefination/Free_Magento_pages.java
@@ -207,7 +207,7 @@ public class Free_Magento_pages extends BaseClass {
 			String url = driver.getCurrentUrl();
 			String URL = "https://www.slideteam.net/free-business-powerpoint-templates/?p=2";
 			if (URL.equals(url)) {
-				Thread.sleep(10000);
+				Thread.sleep(4000);
 				select_ppt = wait.until(
 				ExpectedConditions.elementToBeClickable(By.xpath("//img[@title='Belarus map with flag colour']")));
 		                select_ppt.click();
