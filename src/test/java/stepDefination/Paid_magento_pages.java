a9d71ebcf2e95f374913002c50b3fc842cf12632
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-05-31 15:14:32 +0530

    Update Paid_magento_pages.java

--- src/test/java/stepDefination/Paid_magento_pages.java
+++ src/test/java/stepDefination/Paid_magento_pages.java
@@ -76,7 +76,7 @@ public class Paid_magento_pages extends BaseClass {
 
 	@Then("^click on Any of page number and verify the popular ppts$")
 	public void click_on_Any_of_page_number_and_verify_the_popular_ppts() throws Throwable {
-		/*try {
+		try {
 			Thread.sleep(3000);
 			// click on 3rd page
 			page_number = wait.until(ExpectedConditions
@@ -93,9 +93,10 @@ public class Paid_magento_pages extends BaseClass {
 			Assert.assertTrue("No product is displayed", product_size > 0);
 		} catch (NoSuchElementException e) {
 
-		}*/
+		}
 		
-		for (int second = 0;; second++) {
+		// code for infinite scrolling
+		/*for (int second = 0;; second++) {
 			if (second >= 30) {
 				break;
 			}
@@ -112,13 +113,13 @@ public class Paid_magento_pages extends BaseClass {
 				break;
 			}
 
-		}
+		}*/
 
 	}
 
 	@Then("^click on any of one Ppt$")
 	public void click_on_any_of_one_Ppt() throws Throwable {
-		/*try {
+		try {
 			Thread.sleep(3000);
 			select_ppt = wait.until(ExpectedConditions.elementToBeClickable(
 					By.xpath("//img[@title='Competitive analysis template 5 comparison table powerpoint guide']")));
@@ -126,7 +127,7 @@ public class Paid_magento_pages extends BaseClass {
 			select_ppt.click();
 		} catch (NoSuchElementException e) {
 
-		}*/
+		}
 	}
 
 	@Then("^verify the user will navigation on cottect ppt page$")
@@ -227,7 +228,7 @@ public class Paid_magento_pages extends BaseClass {
 	@Then("^Click on any of page number$")
 	public void click_on_any_of_page_number() throws Throwable {
 		// click on 4th page
-		/*try {
+		try {
 			page_number = wait.until(ExpectedConditions
 					.elementToBeClickable(By.xpath("//div[3]//div[1]//div[1]//div[1]//ul[1]//li[4]//a[1]//span[2]")));
 			pageNumber = page_number.getText();
@@ -242,8 +243,11 @@ public class Paid_magento_pages extends BaseClass {
 			Assert.assertTrue("No product is displayed", product_size > 0);
 		} catch (NoSuchElementException e) {
 
-		}*/
-		for (int second = 0;; second++) {
+		}
+		
+		// code for infinite scrolling
+		
+		/*for (int second = 0;; second++) {
 			if (second >= 30) {
 				break;
 			}
@@ -259,13 +263,13 @@ public class Paid_magento_pages extends BaseClass {
 				break;
 			}
 
-		}
+		}*/
 	}
 
 	@Then("^Click on any of ppt$")
 	public void click_on_any_of_ppt() throws Throwable {
 
-		/*try {
+		try {
 			Thread.sleep(3000);
 			select_ppt = wait.until(ExpectedConditions.elementToBeClickable(
 					By.xpath("//img[@title='Strategic Management Planning Process Powerpoint Presentation Slides']")));
@@ -273,7 +277,7 @@ public class Paid_magento_pages extends BaseClass {
 			select_ppt.click();
 		} catch (NoSuchElementException e) {
 
-		}*/
+		}
 	}
 
 	@Then("^Verify the navigation on cottect ppt page$")
